42cceef1f731e0fe784276aa609105448de5d473
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-12-02 23:19:34 +0300

    use webpack as module bundler for the project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Rainforest Connection Audio Streaming Promo Site
+
+To run webpack type:
+
+```sh
+$ npm run prod
+```
---
+++ src/app.js
@@ -0,0 +1,5 @@
+/* ----- styles ----- */
+require('./less/all.less');
+
+/* ----- library ----- */
+module.exports = window.jQuery = window.$ = require('./js/lib/jquery-2.1.4.min.js');
