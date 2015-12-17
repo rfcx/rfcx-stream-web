73d4a6b04547d3c8e944b4d3da0133d0559e65a7
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-12-18 00:06:29 +0300

    add some ui

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@ var queue = {
     this.bindEvents();
   },
   createChain: function() {
+    this.list = [];
     var def = this.requestToken()
       .then(function(data){
         this.token = data.token;
@@ -19,14 +20,10 @@ var queue = {
         return this.requestData();
       }.bind(this))
       .then(function(res) {
-        if (res && res.length) {
-          // add new urls to local array
-          this.refreshList(res);
-        }
+        // add new urls to local array
+        this.refreshList(res);
         // after success send audio files to audio object
         this.sendAudio();
-        // and trigger start event
-        this.triggerStart();
       }.bind(this));
 
     def.fail(function() {
@@ -36,6 +33,7 @@ var queue = {
   },
   bindEvents: function() {
     $(audio).on('playbackended', this.prepareNext.bind(this));
+    $(audio).on('reset', this.createChain.bind(this));
   },
   requestToken: function() {
     return $.ajax({
@@ -58,6 +56,7 @@ var queue = {
     })
   },
   refreshList: function(data) {
+    console.log('RFCX | Received from server:', data);
     // iterate through all new items in inverse order
     for (var i = data.length-1; i >= 0; i--) {
       var item = data[i];
@@ -84,10 +83,6 @@ var queue = {
     $(this).trigger('newurl', {
       urls: this.list
     });
-  },
-  triggerStart: function() {
-    // sent event to audio object to start playback
-    $(this).trigger('start');
   }
 };
 
@@ -98,27 +93,29 @@ var audio = {
   list: [],
   // current index to play
   index: 1,
+  currentAudioNode: undefined,
+  isStopped: false,
   init: function() {
     this.bindEvents();
   },
   bindEvents: function() {
     // listen to the queue object for the new audio files
     $(queue).on('newurl', this._onNewUrls.bind(this));
+    $('#playStopBtn').click(this._onPlayStopBtnClicked.bind(this));
   },
   createAudios: function() {
     // create html audio instances for each of audio file
     // after that all audio files will be requested, cached and ready to play immediately
-    var _this = this;
-    for (var i=0; i < this.urls.length; i++) {
+    var _this     = this,
+        urlsCount = this.urls.length;
+    for (var i=0; i < urlsCount; i++) {
       if (!this.list[i]) {
         (function(index) {
           loadAudioBuffer(_this.urls[index], function (buffer) {
             var source = context.createBufferSource();
             source.buffer = buffer;
             // set loop for last audio
-            if (index == _this.urls.length-1) {
-              source.loop = true;
-            }
+            source.loop = (index == urlsCount-1);
             _this.list[index] = source;
             if (index == 1) {
               _this.startPlayback();
@@ -127,14 +124,17 @@ var audio = {
         })(i);
       }
       else {
-        this.list[i].loop = false;
+        // set loop for last audio
+        this.list[i].loop = (i == urlsCount-1);
       }
     }
   },
   _onEnd: function(ev) {
     console.log('AUDIO | Playback Finished:', ev);
     // if audio is not last in the list then play the next
-    this.playNext();
+    if (!this.isStopped) {
+      this.playNext();
+    }
   },
   _onNewUrls: function(ev, data) {
     if (data && data.urls) {
@@ -145,6 +145,20 @@ var audio = {
       this.createAudios();
     }
   },
+  _onPlayStopBtnClicked: function(ev) {
+    var $this = $(ev.target);
+    if (!$this.hasClass('stopped')) {
+      $(ev.target).addClass('stopped');
+      this.currentAudioNode.stop();
+      this.isStopped = true;
+    }
+    else {
+      $(ev.target).removeClass('stopped');
+      this.isStopped = false;
+      this.list = [];
+      $(this).trigger('reset');
+    }
+  },
   playNext: function() {
     var toPlayNewSong = false;
     if(this.index + 1 < this.list.length) {
@@ -152,27 +166,50 @@ var audio = {
       toPlayNewSong = true;
     }
     // if audio was last in the list then play it again
-    console.log('play', this.index, this.list[this.index], this.urls[this.index]);
+    console.log('play', this.index+1, this.list[this.index], this.urls[this.index]);
     if (toPlayNewSong) {
       this.list[this.index].onended = this._onEnd.bind(this);
       this.list[this.index].start(0.0);
       this.list[this.index].connect(analyser);
+      this.currentAudioNode = this.list[this.index];
     }
     // trigger event to queue to request new audios from server
     $(this).trigger('playbackended');
   },
   startPlayback: function() {
     // start playing from the second audio file
-    console.log('play', 1, this.list[this.index], this.urls[this.index]);
-    this.list[1].connect(analyser);
+    console.log('play', 2, this.list[this.index], this.urls[this.index]);
     this.list[1].onended = this._onEnd.bind(this);
+    this.list[1].connect(analyser);
     this.list[1].start(0.0);
+    this.currentAudioNode = this.list[1];
 
     window.requestAnimationFrame(draw);
+  }
+};
+
+var clock = {
+  init: function() {
+    this.startTime();
+  },
+  startTime: function() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = this.checkTime(m);
+    s = this.checkTime(s);
+    $('#clock').text(h + ":" + m + ":" + s);
+    setTimeout(this.startTime.bind(this), 500);
+  },
+  checkTime: function(i) {
+    if (i < 10) {i = "0" + i} // add zero in front of numbers < 10
+    return i;
   }
 };
 
 $(function() {
   audio.init();
   queue.init();
+  clock.init();
 });
