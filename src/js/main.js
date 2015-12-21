722bf97d446c06299f34514ca3b5ff436225f262
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-12-21 23:41:32 +0300

    create slideshow

--- src/js/main.js
+++ src/js/main.js
@@ -233,8 +233,48 @@ var clock = {
   }
 };
 
+var slideshow = {
+  init: function() {
+    this.bindEvents();
+    this.initCarousel();
+  },
+  bindEvents: function() {
+    $('#btnShowCarousel').click(this.onBtnShowClicked.bind(this));
+    $('#btnHideCarousel').click(this.onBtnHideClicked.bind(this));
+  },
+  onBtnShowClicked: function() {
+    this.showCarousel();
+  },
+  onBtnHideClicked: function() {
+    this.hideCarousel();
+  },
+  showCarousel: function() {
+    $('#btnShowCarousel').hide();
+    $('#btnHideCarousel').show();
+    $('#imgSlideshow').fadeIn().slick('setPosition').slick('slickPlay');
+  },
+  hideCarousel: function() {
+    $('#btnHideCarousel').hide();
+    $('#imgSlideshow').fadeOut('fast').slick('slickPause');
+    $('#btnShowCarousel').show();
+  },
+  initCarousel: function() {
+    $('#imgSlideshow').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      speed: 700,
+      fade: true,
+      cssEase: 'linear',
+      autoplaySpeed: 5000
+    });
+  }
+};
+
 $(function() {
   audio.init();
   queue.init();
   clock.init();
+  slideshow.init();
 });
