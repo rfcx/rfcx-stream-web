87f317490ef05ecf570205c7497340a3104aa7dd
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-03-11 22:37:37 +0300

    use keypad for login

--- src/js/login.js
+++ src/js/login.js
@@ -1,22 +1,48 @@
 "use strict";
 
 var login = {
-  $info: $('#formInfo'),
+  $info: $('#passwordErrorLabel'),
   $form: $('#loginForm'),
+  $input: $('#passwordInput'),
   $submitBtn: $('#btnSubmitLoginForm'),
   init: function() {
     this.bindEvents();
   },
   bindEvents: function() {
     this.$form.submit(this.onSubmit.bind(this));
+    this.$form.on('click touchstart', '.js-keypad-btn',       this.onKeypadBtnClicked.bind(this));
+    this.$form.on('click touchstart', '.js-keypad-backspace', this.onKeypadBackspaceClicked.bind(this));
   },
   onSubmit: function(ev) {
     ev.preventDefault();
+    if (!this.$input.val().length) {
+      return;
+    }
+    this.clearMessage();
+    this.$input.removeClass('error');
     this.disableButton();
     this.clearMessage();
     this.savePassphrase();
     this.checkPassword();
     return false;
+  },
+  onKeypadBtnClicked: function(ev) {
+    var $this = $(ev.target),
+        val   = $this.attr('data-value');
+    this.clearMessage();
+    this.$input.removeClass('error');
+    this.$input.val(this.$input.val() + val);
+    ev.preventDefault();
+  },
+  onKeypadBackspaceClicked: function(ev) {
+    var currentVal = this.$input.val();
+    if (!currentVal.length) {
+      return;
+    }
+    this.clearMessage();
+    this.$input.removeClass('error');
+    this.$input.val(currentVal.substr(0, currentVal.length-1));
+    ev.preventDefault();
   },
   enableButton: function() {
     this.$submitBtn.prop('disabled', false);
@@ -25,10 +51,24 @@ var login = {
     this.$submitBtn.prop('disabled', true);
   },
   savePassphrase: function() {
-    window.rfcxPassphrase = $('#passphraseInput').val();
+    window.rfcxPassphrase = this.$input.val();
   },
   checkPassword: function() {
-    queue.checkPassword();
+    queue.checkPassword()
+      .done(function () {
+        this.hideOverlay();
+        this.enableButton();
+      }.bind(this))
+      .fail(function(err) {
+        this.enableButton();
+        if (err.status == 401) {
+          this.$input.addClass('error');
+          this.setMessage('Invalid Password');
+        }
+        else {
+          this.setMessage('Error while initialization');
+        }
+      }.bind(this));
   },
   hideOverlay: function() {
     $('#loginOverlay').fadeOut('fast');
