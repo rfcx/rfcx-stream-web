e666c20eeebf0b770e46d120e65b1bf5b3ee373c
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-03-14 13:34:49 +0300

    show guardian's timezone time and show timezone label (related to #3) hardcode Amazon timezone

--- src/js/clock.js
+++ src/js/clock.js
@@ -14,6 +14,11 @@ var clock = {
   },
   initTime: function(date) {
     this.time = new Date(date);
+    // get actual hours which were in ISO time (GMT 00:00))
+    this.time = new Date(this.time.getTime() + (this.time.getTimezoneOffset() * 60000));
+    // substract guardian's timezone offset. hardcode Amazon Timezone (GMT -04:00)
+    this.time.setHours(this.time.getHours() - 4);
+    setTimeout(this.setTimezone, 1000);
     this.startTimer();
   },
   startTimer: function() {
@@ -34,6 +39,7 @@ var clock = {
     this.m = this.time.getMinutes();
     this.s = this.time.getSeconds();
 
+    this.h = this.checkTime(this.h);
     this.m = this.checkTime(this.m);
     this.s = this.checkTime(this.s);
   },
@@ -42,6 +48,9 @@ var clock = {
   },
   render: function() {
     $('#clock').text(this.h + ":" + this.m + ":" + this.s);
+  },
+  setTimezone: function() {
+    $('#timezone').text('Amazon Standard Time');
   },
   checkTime: function(i) {
     if (i < 10) {i = "0" + i} // add zero in front of numbers < 10
